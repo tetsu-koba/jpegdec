99cc80798d532eac43808b837b9a571ee90aa6ac
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-04-26 21:29:38 +0900

    Initial commit

---
+++ src/jpegdec.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const os = std.os;
+const assert = std.debug.assert;
+
+// Import the TurboJPEG library.
+const tj = @cImport({
+    @cInclude("turbojpeg.h");
+});
+
+pub fn decodeJpegToI422(jpeg_data: []const u8, i422_data: []u8, width: u32, height: u32) !void {
+    // Create a TurboJPEG decompressor instance.
+    const tj_instance = tj.tjInitDecompress();
+    if (tj_instance == null) {
+        return error.TurboJpegInitDecompressFailed;
+    }
+    defer _ = tj.tjDestroy(tj_instance);
+
+    // Get the JPEG image dimensions and pixel format.
+    var jpeg_width: c_int = 0;
+    var jpeg_height: c_int = 0;
+    var jpeg_subsamp: c_int = 0;
+    var jpeg_colorspace: c_int = 0;
+    if (tj.tjDecompressHeader3(tj_instance, &jpeg_data[0], jpeg_data.len, &jpeg_width, &jpeg_height, &jpeg_subsamp, &jpeg_colorspace) != 0) {
+        return error.TurboJpegDecompressHeaderFailed;
+    }
+
+    // Check if the given width and height match the JPEG header information.
+    if (jpeg_width != width or jpeg_height != height) {
+        return error.ImageDimensionsMismatch;
+    }
+
+    // Check if the pixel format is YUV422.
+    if (jpeg_subsamp != tj.TJSAMP_422) {
+        return error.InvalidPixelFormat;
+    }
+
+    // Decompress the JPEG image data to the I422 format.
+    if (tj.tjDecompressToYUV2(tj_instance, &jpeg_data[0], jpeg_data.len, &i422_data[0], jpeg_width, 0, jpeg_height, 0) != 0) {
+        return error.TurboJpegDecompressToYUV2Failed;
+    }
+}
