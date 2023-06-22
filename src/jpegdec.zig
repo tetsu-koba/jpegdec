e7471c24cce9d81927a7c7ae8cd566ba670def21
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-06-22 15:15:24 +0900

    Add struct JpegDec

--- src/jpegdec.zig
+++ src/jpegdec.zig
@@ -41,3 +41,50 @@ pub fn decodeJpegToI422(jpeg_data: []const u8, i422_data: []u8, width: u32, heig
         return error.TurboJpegDecompressToYUV2Failed;
     }
 }
+
+pub const JpegDec = struct {
+    instance: tj.tjhandle,
+
+    const Self = @This();
+
+    pub fn init() !JpegDec {
+        const instance = tj.tjInitDecompress();
+        if (instance == null) {
+            return error.TurboJpegInitDecompressFailed;
+        }
+        return JpegDec{ .instance = instance };
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = tj.tjDestroy(self.instance);
+    }
+
+    pub fn decodeToI422(self: *Self, jpeg_data: []const u8, i422_data: []u8, width: u32, height: u32) !void {
+        // Get the JPEG image dimensions and pixel format.
+        var jpeg_width: c_int = 0;
+        var jpeg_height: c_int = 0;
+        var jpeg_subsamp: c_int = 0;
+        var jpeg_colorspace: c_int = 0;
+        if (tj.tjDecompressHeader3(self.instance, &jpeg_data[0], jpeg_data.len, &jpeg_width, &jpeg_height, &jpeg_subsamp, &jpeg_colorspace) != 0) {
+            std.log.err("tjDecompressHeader3: {s}", .{std.mem.sliceTo(tj.tjGetErrorStr2(self.instance), 0)});
+            return error.TurboJpegDecompressHeaderFailed;
+        }
+
+        // Check if the given width and height match the JPEG header information.
+        if (jpeg_width != width or jpeg_height != height) {
+            return error.ImageDimensionsMismatch;
+        }
+
+        // Check if the pixel format is YUV422.
+        if (jpeg_subsamp != tj.TJSAMP_422) {
+            return error.InvalidPixelFormat;
+        }
+
+        // Decompress the JPEG image data to the I422 format.
+        if (tj.tjDecompressToYUV2(self.instance, &jpeg_data[0], jpeg_data.len, &i422_data[0], jpeg_width, 1, jpeg_height, 0) != 0) {
+            // JPEG data is frequently corrupted in MJPEG
+            std.log.info("tjDecompressToYUV2: {s}", .{std.mem.sliceTo(tj.tjGetErrorStr2(self.instance), 0)});
+            return error.TurboJpegDecompressToYUV2Failed;
+        }
+    }
+};
--- src/jpegdec_test.zig
+++ src/jpegdec_test.zig
@@ -30,3 +30,30 @@ test "decodeJpegToI422" {
 
     try expect(mem.eql(u8, expected_i422_data, i422_data_buf[0..]));
 }
+
+test "JpegToI422" {
+    const allocator = std.heap.page_allocator;
+
+    const input_jpeg_path = "testfiles/test001.jpeg";
+    const expected_output_path = "testfiles/test001.i422";
+
+    var file1 = try std.fs.cwd().openFile(input_jpeg_path, .{});
+    defer file1.close();
+    const jpeg_data = try file1.readToEndAlloc(allocator, 4 * 1024 * 1024);
+    defer allocator.free(jpeg_data);
+
+    var file2 = try std.fs.cwd().openFile(expected_output_path, .{});
+    defer file2.close();
+    const expected_i422_data = try file2.readToEndAlloc(allocator, 4 * 1024 * 1024);
+    defer allocator.free(expected_i422_data);
+    var jp = try jpegdec.JpegDec.init();
+    defer jp.deinit();
+
+    // Test decoding
+    const width: u32 = 160;
+    const height: u32 = 90;
+    var i422_data_buf: [width * height * 2]u8 = undefined;
+    try jp.decodeToI422(jpeg_data, &i422_data_buf, width, height);
+
+    try expect(mem.eql(u8, expected_i422_data, i422_data_buf[0..]));
+}
