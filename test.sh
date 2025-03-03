52935e336cb52994470557ec1ed1a0b34f5572df
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2025-03-03 15:29:27 +0900

    Test on macos

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
+case "$OSTYPE" in
+    darwin*) case "$HOSTTYPE" in
+		 arm64) LIBDIR=/opt/homebrew/lib;INCLUDE=/opt/homebrew/include;;
+		 *)	LIBDIR=/usr/local/lib;INCLUDE=/usr/local/include;;
+	     esac ;;
+     *) LIBDIR=/usr/lib;INCLUDE=/usr/include;;
+esac
+
 if [ $# -eq 0 ]; then
     OPTS=-Doptimize=Debug
 else
     OPTS=-Doptimize=$1
 fi
-OPTS="$OPTS -I /opt/libjpeg-turbo/include/ -L /opt/libjpeg-turbo/lib64/"
 for i in src/*_test.zig; do
-    zig test $OPTS $i -lc -lturbojpeg
+    zig test $OPTS $i -I$INCLUDE -L$LIBDIR -lturbojpeg -lc
 done
 
